7dff56b959e4a0ea7b04c466e7eecdfe8d17d656
Acan <xie.zeyu20@gmail.com>
2023-09-20 14:24:06 +0800

    .

--- examples/time.js
+++ examples/time.js
@@ -1,26 +1,43 @@
-//Basic
+// Get concrete time and date info
 
-console.log("Basic:")
+console.log("1. Get concrete time and date info:")
 
-const time=new Date()
-console.log(time)
+const now = new Date()
+console.log(now)
 
-const year=time.getFullYear()
-const month=time.getMonth()+1
-const date=time.getDate()
+const year = now.getFullYear()
+const month = now.getMonth() + 1
+const date = now.getDate()
 console.log(year, month, date)
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const day=time.getDay()
+const day = now.getDay()
 console.log(day, daysOfWeek[day])
 
-const hour=time.getHours()
-const minute=time.getMinutes()
-const second=time.getSeconds()
+const hour = now.getHours()
+const minute = now.getMinutes()
+const second = now.getSeconds()
 console.log(`${hour}:${minute}:${second}`)
 
 console.log("\n")
 
-//Certain Position
+// Yesterday
 
-console.log(address)
+console.log("2. Yesterday:")
+
+const yesterday = new Date(now - 24 * 60 * 60 * 1000)
+console.log(yesterday)
+
+console.log("\n")
+
+// Customize Time Zone
+
+console.log("3. Customize Time Zone: (Europe/Stockholm)")
+
+const timeZone = 'Europe/Stockholm';
+const options = { timeZone, hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+const formatter = new Intl.DateTimeFormat('en-US', options);
+const formattedDate = formatter.format(now);
+
+console.log(formattedDate)
+console.log("\n")
